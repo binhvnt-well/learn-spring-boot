b59f74d5d60d066d3ad10352d5bcefb52007e64e
BagWell <bagwell@macbook-pro-cua-bagwell.local>
2025-01-08 19:31:43 +0700

    su dung lombok trong spring boot

--- src/main/java/com/devteria/identity_sevice/dto/request/UserCreationRequest.java
+++ src/main/java/com/devteria/identity_sevice/dto/request/UserCreationRequest.java
@@ -1,71 +1,24 @@
 package com.devteria.identity_sevice.dto.request;
 
 import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
+//Refer https://www.youtube.com/watch?v=3AIjB50cRzU&ab_channel=Devteria
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserCreationRequest {
-    private String username;
+    String username;
 
     @Size(min = 6, max = 16, message = "INVALID_PASSWORD")
-    private String password;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phone;
-    private String dob;
+    String password;
+    String firstName;
+    String lastName;
+    String email;
+    String phone;
+    String dob;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
 }
